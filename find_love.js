02007206429a89fa2f350db6f3ae27024a72205b
Andrew Dymond <andy.dymond@gmail.com>
2018-05-10 10:32:00 -0600

    Rename variables for clarity

--- find_love.js
+++ find_love.js
@@ -1,23 +1,22 @@
 class lovetangle {
   constructor(attrs) {
-    this.lx = attrs.left_x
-    this.by = attrs.bottom_y
+    this.left_x = attrs.left_x
+    this.bottom_y = attrs.bottom_y
     this.width = attrs.width
     this.height = attrs.height
   }
 }
 
 function get_intersection(lt1, lt2) {
-  let ilx = (lt1.lx > lt2.lx) ? lt1.lx : lt2.lx
-  let iby = (lt1.by > lt2.by) ? lt1.by : lt2.by
-  let iw = (lt2.lx + lt2.width > lt1.lx + lt1.width) ? (lt1.lx + lt1.width) - ilx : lt2.width
-  let ih = (lt2.by + lt2.height > lt1.by + lt1.height) ? (lt1.by + lt1.height) - iby : lt2.height
-  return {left_x: ilx, bottom_y: iby, width: iw, height: ih}
+  let ileft_x = (lt1.left_x > lt2.left_x) ? lt1.left_x : lt2.left_x
+  let ibottom_y = (lt1.bottom_y > lt2.bottom_y) ? lt1.bottom_y : lt2.bottom_y
+  let iw = (lt2.left_x + lt2.width > lt1.left_x + lt1.width) ? (lt1.left_x + lt1.width) - ileft_x : lt2.width
+  let ih = (lt2.bottom_y + lt2.height > lt1.bottom_y + lt1.height) ? (lt1.bottom_y + lt1.height) - ibottom_y : lt2.height
+  return {left_x: ileft_x, bottom_y: ibottom_y, width: iw, height: ih}
 }
 
-
 function find_overlap(lt1, lt2) {
-  if(lt1.lx >= lt2.lx + lt2.width || lt1.by >= lt2.by + lt2.height) {
+  if(lt1.left_x >= lt2.left_x + lt2.width || lt1.bottom_y >= lt2.bottom_y + lt2.height) {
     return new lovetangle({left_x: 0, bottom_y: 0, width: 0, height: 0})
   } else {
     let intersection = get_intersection(lt1, lt2)

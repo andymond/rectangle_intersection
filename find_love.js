e48ee3ce51c31622fd4669934c40be3865369e46
Andrew Dymond <andy.dymond@gmail.com>
2018-05-10 10:00:09 -0600

    Rm get corners & add get intersection function

--- find_love.js
+++ find_love.js
@@ -7,15 +7,21 @@ class lovetangle {
   }
 }
 
-function get_corners(lt) {
-  let btm_left = [lx, by]
-  let top_left = [lx, (by + h)]
-  let top_right = [(lx + w), (by + h)]
-  let btm_right = [(lx + w), by]
-  return [ btm_left, top_left, top_right, btm_right ]
+function get_intersection(lt1, lt2) {
+  lt1.lx > lt2.lx ? let ilx = lt1.lx : let ilx = lt2.lx
+  lt1.by > lt2.by ? let iby = lt1.by : let iby = lt2.by
+  lt2.lx + lt2.width > lt1.lx + lt1.width ? let iw = (lt1.lx + w) - ilx : let iw = lt2.width
+  lt2.by + lt2.height > lt1.by + lt1.height ? let ih = (lt1.by + h) - iby : let ih = lt2.height
+
+  return [ilx, iby, iw, ih]
 }
 
 
 function find_intersection(lt1, lt2) {
-
+  if(lt1.lx > lt2.lx + lt2.width || lt1.by > lt2.by + lt2.height) {
+    return new lovetangle(0, 0, 0, 0)
+  } else {
+    let intersection = get_intersection(lt1, lt2)
+    return new lovetangle(intersection.ilx, intersection.iby, intersection.iw, intersection.ih)
+  }
 }

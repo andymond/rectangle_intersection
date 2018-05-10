156d3cc51767ce9629d9426c7f69bf5e0fa5f8c9
Andrew Dymond <andy.dymond@gmail.com>
2018-05-10 09:19:17 -0600

    Initial commit

---
+++ find_love.js
@@ -0,0 +1,12 @@
+class lovetangle {
+  constructor(lx, by, w, h) {
+    this.lx = lx
+    this.by = by
+    this.width = w
+    this.height = h
+  }
+}
+
+function find_intersection(lt1, lt2) {
+  
+}
